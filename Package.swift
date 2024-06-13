b2b6b03e4c523102f097d9863fa3a21a48aa3810
ArkadiGiniApps <arkadiy@gini-apps.com>
2024-06-13 18:27:45 +0300

    main - update package setup

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ package.products    = [
     .library(name: "SoliticsLibrary", targets: ["SoliticsPackage"])
 ]
 package.dependencies = [
-    .package(id: "swift.solitics", from: "3.0.2")
+    .package(id: "swift.solitics", from: "3.0.3")
 ]
 package.targets     = [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
