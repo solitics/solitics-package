3693460115fd9d503f5412e24752344cf191d503
ArkadiGiniApps <arkadiy@gini-apps.com>
2024-08-06 10:19:21 +0300

    Update README.md

--- Sources/dummy.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Accessor.swift
-//  SoliticsAccessor
-//
-//  Created by Arkadi Yoskovitz on 26/12/2023.
-//  Copyright © 2023 com.colitics. All rights reserved.
-//
-
-// This is a dummy placeholder file
